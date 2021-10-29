4e597a1f8b6c6d406592c2744923208fb342b6a1
selebruno <selebruno@hotmail.com>
2021-10-29 12:41:50 -0300

    fix deploy

--- client/src/index.js
+++ client/src/index.js
@@ -12,8 +12,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const REACT_APP_API = process.env
-axios.defaults.baseURL = REACT_APP_API 
-// || "http://localhost:3001" pasar a local host para probar en desarrollo
+axios.defaults.baseURL = REACT_APP_API || "http://localhost:3001";
 
 ReactDOM.render(
   <Provider store={store}>
